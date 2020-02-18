8bb8d2a474eda056c4f3a038119f07dee8cb9401
Javier de la Llave <jdelallalor@gmail.com>
2020-02-18 13:17:52 +0100

    Burbuja optimizada

--- src/algoritmosordenacion/AlgoritmosOrdenacion.java
+++ src/algoritmosordenacion/AlgoritmosOrdenacion.java
@@ -11,25 +11,47 @@ package algoritmosordenacion;
  */
 public class AlgoritmosOrdenacion {
 
-    int[] nums = {13, 27, 455, 621, 23, 1, 15};
+    int[] nums = {3,1,5,2,4};
 
-    public int[] ordenacionBurbuja(int[] numeros) {
+    //Algoritmo de la burbuna optimizado teniendo en cuenta que si no hay intercambio
+    //de números es que está ordanado y que a n vueltas los n números mayores están ordenados
+    //al final
+    public int[] burbuja(int[] numeros) {
+        
+        int aux = 0;//Lo utilizamos para hacer el intercambio
 
-        int aux = 0;
-        boolean cambio = true;
-
-        while (cambio) {
-            cambio = false;
-            for (int j = 0; j < numeros.length - 1; j++) {
-             
+        int nVueltas = numeros.length - 1;
+        while(true) {
+            boolean ordenado = true;
+            for (int j = 0; j < nVueltas; j++) {
                 if (numeros[j] > numeros[j + 1]) {
-                    aux = numeros[j ];
-                    numeros[j] = numeros[j+1];
-                    numeros[j + 1] = aux;
-                    cambio = true;
-
+                    aux = numeros[j + 1];
+                    numeros[j + 1] = numeros[j];
+                    numeros[j] = aux;
+                    ordenado = false;
                 }
             }
+            System.out.println(nVueltas);
+            if (ordenado || nVueltas<=1) {
+                return numeros;
+            }
+            
+            nVueltas--;
+        }
+       
+    }
+
+
+    //Ordenación por insercción directa
+    public int[] insercionDirecta(int[] numeros) {
+        for (int i = 1; i < numeros.length; i++) {
+            int aux = numeros[i];
+            int j = i;
+            while (j > 0 && numeros[j - 1] > aux) {
+                numeros[j] = numeros[j - 1];
+                j--;
+            }
+            numeros[j] = aux;
         }
         return numeros;
     }
@@ -44,9 +66,9 @@ public class AlgoritmosOrdenacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        AlgoritmosOrdenacion miAlgoritmo = new AlgoritmosOrdenacion();
 
-        miAlgoritmo.imprimir(miAlgoritmo.ordenacionBurbuja(miAlgoritmo.nums));
+        AlgoritmosOrdenacion miAlgoritmo = new AlgoritmosOrdenacion();
+        miAlgoritmo.imprimir(miAlgoritmo.burbuja(miAlgoritmo.nums));
 
     }
 
